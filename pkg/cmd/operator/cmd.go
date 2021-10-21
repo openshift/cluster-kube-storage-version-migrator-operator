39e148bb70d41e11e1af6204386bd792dadf03c5
Luis Sanchez <sanchezl@redhat.com>
2021-10-20 21:51:50 -0400

    cleanup: use deploymentcontroller

--- pkg/cmd/operator/cmd.go
+++ pkg/cmd/operator/cmd.go
@@ -3,17 +3,17 @@ package operator
 import (
 	"context"
 
+	"github.com/openshift/library-go/pkg/controller/controllercmd"
 	"github.com/spf13/cobra"
 
-	"github.com/openshift/library-go/pkg/controller/controllercmd"
-
+	"github.com/openshift/cluster-kube-storage-version-migrator-operator/pkg"
 	"github.com/openshift/cluster-kube-storage-version-migrator-operator/pkg/operator"
 	"github.com/openshift/cluster-kube-storage-version-migrator-operator/pkg/version"
 )
 
 func NewOperator() *cobra.Command {
 	cmd := controllercmd.NewControllerCommandConfig(
-		operator.OperatorNamespace,
+		pkg.OperatorNamespace,
 		version.Get(),
 		operator.RunOperator,
 	).NewCommandWithContext(context.TODO())
