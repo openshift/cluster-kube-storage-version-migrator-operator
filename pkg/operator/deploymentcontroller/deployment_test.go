5d4d1efca85876404bfbbfd10b3c18f1ab4ebcc8
Luis Sanchez <sanchezl@redhat.com>
2024-11-01 13:41:51 -0400

    fix operand log level handling

--- pkg/operator/deploymentcontroller/deployment_test.go
+++ pkg/operator/deploymentcontroller/deployment_test.go
@@ -12,29 +12,55 @@ import (
 func Test_setOperandLogLevel(t *testing.T) {
 
 	testCases := []struct {
+		name     string
 		logLevel operatorv1.LogLevel
 		command  []string
+		args     []string
 		expected []string
 	}{
 		{
+			name:     "CmdAppend",
 			logLevel: operatorv1.Debug,
 			command:  []string{"arg0", "arg1", "arg2"},
 			expected: []string{"arg0", "arg1", "arg2", "--v=4"},
 		},
 		{
+			name:     "CmdReplaceMid",
 			logLevel: operatorv1.Trace,
 			command:  []string{"arg0", "--v=2", "arg2"},
 			expected: []string{"arg0", "--v=6", "arg2"},
 		},
 		{
+			name:     "CmdReplaceEnd",
 			logLevel: operatorv1.TraceAll,
 			command:  []string{"arg0", "arg1", "--v=2"},
 			expected: []string{"arg0", "arg1", "--v=8"},
 		},
+		{
+			name:     "ArgsAppend",
+			logLevel: operatorv1.Debug,
+			command:  []string{"arg0"},
+			args:     []string{"arg1", "arg2", "arg3"},
+			expected: []string{"arg0", "arg1", "arg2", "arg3", "--v=4"},
+		},
+		{
+			name:     "ArgsReplaceMid",
+			logLevel: operatorv1.Trace,
+			command:  []string{"arg0"},
+			args:     []string{"arg1", "--v=2", "arg3"},
+			expected: []string{"arg0", "arg1", "--v=6", "arg3"},
+		},
+		{
+			name:     "ArgsReplaceEnd",
+			logLevel: operatorv1.TraceAll,
+			command:  []string{"arg0"},
+			args:     []string{"arg1", "arg2", "--v=2"},
+			expected: []string{"arg0", "arg1", "arg2", "--v=8"},
+		},
 	}
 
 	for _, tc := range testCases {
-		t.Run("", func(t *testing.T) {
+		t.Run(tc.name, func(t *testing.T) {
 			spec := &operatorv1.OperatorSpec{
 				LogLevel: tc.logLevel,
 			}
@@ -43,17 +69,20 @@ func Test_setOperandLogLevel(t *testing.T) {
 					Containers: []corev1.Container{{
 						Name:    "migrator",
 						Command: tc.command,
+						Args:    tc.args,
 					}},
 				}}},
 			}
 			_ = setOperandLogLevel(spec, deployment)
-			if !cmp.Equal(deployment.Spec.Template.Spec.Containers[0].Command, tc.expected) {
-				t.Fatal(cmp.Diff(deployment.Spec.Template.Spec.Containers[0].Command, tc.expected))
+			c := deployment.Spec.Template.Spec.Containers[0]
+			actual := append(append([]string{}, c.Command...), c.Args...)
+			if !cmp.Equal(tc.expected, actual) {
+				t.Fatal(cmp.Diff(tc.expected, actual))
 			}
 		})
 	}
 
-	t.Run("", func(t *testing.T) {
+	t.Run("OtherContainer", func(t *testing.T) {
 		spec := &operatorv1.OperatorSpec{LogLevel: operatorv1.Debug}
 		deployment := &appsv1.Deployment{
 			Spec: appsv1.DeploymentSpec{Template: corev1.PodTemplateSpec{Spec: corev1.PodSpec{
